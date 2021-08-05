c1ced891d04aa0a61bdb83e9cc2e73d650c82fce
Bohdan Vereshchak <veres.bs@gmail.com>
2021-08-05 16:34:01 +0300

    Add project files.

---
+++ src/ApplicationCore/Services/DeliveryOrderProcessor.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Services
+{
+	class DeliveryOrderProcessor
+	{
+		const string AzureFunctionURL = "https://deliveryorderprocessor.azurewebsites.net/api/DeliveryOrderProcessor";
+
+		public async Task<bool> Process(Order order)
+		{
+			var deliveryOrder = new
+			{
+				Id = order.Id,
+				ShipToAddress = order.ShipToAddress,
+				OrderItems = order.OrderItems.Select(oi => new
+				{
+					ItemId = oi.ItemOrdered.CatalogItemId,
+					ProductName = oi.ItemOrdered.ProductName,
+					Units = oi.Units
+				}),
+				FinalPrice = order.OrderItems.Select(oi => oi.UnitPrice * oi.Units).Sum()
+			};
+
+			using (var client = new HttpClient())
+			using (var request = new HttpRequestMessage(HttpMethod.Post, AzureFunctionURL))
+			using (HttpContent httpContent = CreateHttpContent(deliveryOrder))
+			{
+				request.Content = httpContent;
+
+				using (HttpResponseMessage response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead))
+				{
+					return response.IsSuccessStatusCode;
+				}
+			}
+		}
+
+		private HttpContent CreateHttpContent(object content)
+		{
+			var ms = new MemoryStream();
+			SerializeJsonIntoStream(content, ms);
+			ms.Seek(0, SeekOrigin.Begin);
+			var httpContent = new StreamContent(ms);
+			httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+			return httpContent;
+		}
+
+		private void SerializeJsonIntoStream(object value, Stream stream)
+		{
+			using (var sw = new StreamWriter(stream, new UTF8Encoding(false), 1024, true))
+			using (var jtw = new JsonTextWriter(sw) { Formatting = Formatting.None })
+			{
+				var js = new JsonSerializer();
+				js.Serialize(jtw, value);
+				jtw.Flush();
+			}
+		}
+	}
+}
---
+++ src/ApplicationCore/Services/OrderItemsReserver.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Services
+{
+	class OrderItemsReserver
+	{
+		const string ServiceBusConnectionString = "Endpoint=sb://cloudx-bv-final-task.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=k0hBuKxetOmqYxerPaJUmpvZGDVf7cwpfIBjaSOOOF8=";
+		const string QueueName = "cloudx-bv-final-task-queue";
+
+		public async Task Reserve(Order order)
+		{
+			string json = JsonConvert.SerializeObject(order);
+
+			await SendMessageToQueueAsync(json);
+		}
+
+		private async Task SendMessageToQueueAsync(string messageBody)
+		{
+			var queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+			var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+			await queueClient.SendAsync(message);
+			await queueClient.CloseAsync();
+		}
+	}
+}
